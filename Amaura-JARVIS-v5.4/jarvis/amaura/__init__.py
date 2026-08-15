6efed69f755eb4a85459cf11c441cf0e8abb19f6
akshat92008 <dr.akshatsingh720@gmail.com>
2026-08-15 21:31:43 +0530

    fix(arch): install workflow precedence release contracts

--- Amaura-JARVIS-v5.4/jarvis/amaura/__init__.py
+++ Amaura-JARVIS-v5.4/jarvis/amaura/__init__.py
@@ -6,6 +6,7 @@ from jarvis.amaura.integrations import ProviderReceipt
 from jarvis.amaura.models import ApprovalStatus, RiskLevel, TaskState
 from jarvis.amaura.semantic_adapters import install_semantic_adapters
 from jarvis.amaura.semantic_core import install_semantic_core
+from jarvis.amaura.semantic_release_contracts import install_semantic_release_contracts
 from jarvis.amaura.semantic_write_compat import install_semantic_write_compat
 from jarvis.amaura.supervisor import AmauraSupervisor
 from jarvis.amaura.telemetry import OperationalTelemetry
@@ -16,6 +17,7 @@ from jarvis.amaura.telemetry import OperationalTelemetry
 install_semantic_core()
 install_semantic_adapters()
 install_semantic_write_compat()
+install_semantic_release_contracts()
 
 __all__ = [
     "AmauraControlPlane",
