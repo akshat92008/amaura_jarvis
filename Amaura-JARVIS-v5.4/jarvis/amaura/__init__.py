1dbc61cd12d399c9dcb84fd8b664c44aaff4542d
akshat92008 <dr.akshatsingh720@gmail.com>
2026-08-15 21:44:54 +0530

    fix(arch): install directory-list precedence contract

--- Amaura-JARVIS-v5.4/jarvis/amaura/__init__.py
+++ Amaura-JARVIS-v5.4/jarvis/amaura/__init__.py
@@ -6,6 +6,7 @@ from jarvis.amaura.integrations import ProviderReceipt
 from jarvis.amaura.models import ApprovalStatus, RiskLevel, TaskState
 from jarvis.amaura.semantic_adapters import install_semantic_adapters
 from jarvis.amaura.semantic_core import install_semantic_core
+from jarvis.amaura.semantic_list_compat import install_semantic_list_compat
 from jarvis.amaura.semantic_path_normalization import install_semantic_path_normalization
 from jarvis.amaura.semantic_release_contracts import install_semantic_release_contracts
 from jarvis.amaura.semantic_write_compat import install_semantic_write_compat
@@ -19,6 +20,7 @@ install_semantic_core()
 install_semantic_path_normalization()
 install_semantic_adapters()
 install_semantic_write_compat()
+install_semantic_list_compat()
 install_semantic_release_contracts()
 
 __all__ = [
